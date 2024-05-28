b0a055c9ef39f9d32c4f29276a099e5adbe7ae0b
Alex Balev <aleks.balev7899@gmail.com>
2024-05-29 01:14:55 +0200

    added material to the project and created some basic colors css variables

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'order-history',
+    loadComponent: () => import('./order-history/order-history.component'),
+  },
+  {
+    path: '',
+    redirectTo: 'order-history',
+    pathMatch: 'full',
+  },
+];
